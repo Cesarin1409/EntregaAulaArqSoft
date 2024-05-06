4eb98e2f65d130087659288f69dc2da28f917041
mafioletti98 <matemafioletti@gmail.com>
2024-05-06 09:55:48 -0300

    no merge foi retirado a restriçao private/protected

---
+++ Downloads/-AtividadePratica2--master (1)/-AtividadePratica2--master/src/Models/Comissario.java
@@ -0,0 +1,64 @@
+package Models;
+
+public class Comissario extends Pessoa {
+
+    protected String nome;
+    protected long cpf;
+    protected String telefone;
+    protected int idCarteiraComissario;
+    protected String funcaoVoo;
+
+    public Comissario() {
+
+    }
+
+    public Comissario(long cpf, String nome, String telefone, int idCarteiraComissario, String funcaoVoo) {
+
+        this.cpf = cpf;
+        this.nome = nome;
+        this.telefone = telefone;
+        this.idCarteiraComissario = idCarteiraComissario;
+        this.funcaoVoo = funcaoVoo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public long getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(long cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public int getIdCarteiraComissario() {
+        return idCarteiraComissario;
+    }
+
+    public void setIdCarteiraComissario(int idCarteiraComissario) {
+        this.idCarteiraComissario = idCarteiraComissario;
+    }
+
+    public String getFuncaoVoo() {
+        return funcaoVoo;
+    }
+
+    public void setFuncaoVoo(String funcaoVoo) {
+        this.funcaoVoo = funcaoVoo;
+    }
+
+}
---
+++ Downloads/-AtividadePratica2--master (1)/-AtividadePratica2--master/src/Models/Passageiro.java
@@ -0,0 +1,39 @@
+package Models;
+
+public class Passageiro extends Pessoa {
+    protected String numeroPoltrona;
+    protected String numeroCartaoEmbarque;
+
+    public Passageiro() {
+    }
+
+    public Passageiro(long cpf, String nome, String telefone, String numeroPoltrona, String numeroCartaoEmbarque) {
+        super(cpf, nome, telefone);
+        this.numeroPoltrona = numeroPoltrona;
+        this.numeroCartaoEmbarque = numeroCartaoEmbarque;
+    }
+
+    public String getNumeroPoltrona() {
+        return numeroPoltrona;
+    }
+
+    public void setNumeroPoltrona(String numeroPoltrona) {
+        this.numeroPoltrona = numeroPoltrona;
+    }
+
+    public String getNumeroCartaoEmbarque() {
+        return numeroCartaoEmbarque;
+    }
+
+    public void setNumeroCartaoEmbarque(String numeroCartaoEmbarque) {
+        this.numeroCartaoEmbarque = numeroCartaoEmbarque;
+    }
+
+    @Override
+    public String toString() {
+        return "\nPassageiro: " + nome + ",\n cpf: " + cpf + ",\n Numero da poltrona: " + numeroPoltrona
+                + ",\n Telefone: "
+                + telefone + ", Numero Cartao de Embarque: " + numeroCartaoEmbarque + "\n";
+    }
+
+}
