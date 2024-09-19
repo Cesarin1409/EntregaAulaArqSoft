17d904ebb93c2e778ffc7dfdc3313ac4d8ba0577
Cesar Augusto <141377545+Cesarin1409@users.noreply.github.com>
2024-09-18 21:13:50 -0300

    Delete Downloads directory

--- Downloads/ProjetoAPI-20240426T120152Z-001/ProjetoAPI/modelos/Funcionario.cs
+++
@@ -1,6 +0,0 @@
-public class Funcionario
-{
-    public int Id { get; set; }
-    public string? Nome { get; set; }
-    public string? Cargo { get; set; }
-}
--- Downloads/ProjetoAPI-20240426T120152Z-001/ProjetoAPI/modelos/Produto.cs
+++
@@ -1,7 +0,0 @@
-
-public class Produto
-{
-    public int Id { get; set; }
-    public string? Nome { get; set; }
-    public string? Descricao { get; set; }
-}
